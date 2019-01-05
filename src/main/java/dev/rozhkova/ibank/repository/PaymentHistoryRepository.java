3ba558abc84b035aa5f3d80f15f5700531bb63f1
Alex <alekseisamuylov@gmail.com>
2019-01-05 17:32:51 +0300

    Delete PaymentHistoryRepository.java

--- src/main/java/dev/rozhkova/ibank/repository/PaymentHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.rozhkova.ibank.repository;
-
-import dev.rozhkova.ibank.entity.PaymentHistoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentHistoryRepository extends JpaRepository<PaymentHistoryEntity, Long> {
-}
