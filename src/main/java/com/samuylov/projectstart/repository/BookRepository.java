ddaabf43388712c7cccc7b39b83162a3f67831be
Alex <alekseisamuylov@gmail.com>
2019-01-06 12:16:56 +0300

    Resolve merge conflict

--- src/main/java/com/samuylov/projectstart/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.samuylov.projectstart.repository;
-
-import com.samuylov.projectstart.entity.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
-    BookEntity findById(long bookId);
-}
